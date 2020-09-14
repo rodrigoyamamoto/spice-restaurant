239dbcbbec1983abc765778ac9c07f72eb8e1141
Rodrigo Yamamoto <rr.yamamoto@outlook.com>
2020-09-15 00:01:51 +0100

    Project setup to use Areas

--- Spice/Areas/Customer/Controllers/HomeController.cs
+++ Spice/Areas/Customer/Controllers/HomeController.cs
@@ -1,14 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Spice.Models;
+using System.Diagnostics;
 
-namespace Spice.Controllers
+namespace Spice.Areas.Customer.Controllers
 {
+    [Area("Customer")]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
