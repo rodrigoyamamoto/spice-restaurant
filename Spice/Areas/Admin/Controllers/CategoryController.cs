4bab7a6d7cd3eadfe8c9028cd3457850d0c93cf4
Rodrigo Yamamoto <rr.yamamoto@outlook.com>
2020-09-15 05:02:52 +0100

    feat: include create action, create view, create partial view and minor css tweaks

---
+++ Spice/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,32 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Spice.Data;
+
+namespace Spice.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CategoryController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET
+        public async Task<IActionResult> Index()
+        {
+            var categoryList = await _context.Category.ToListAsync();
+            return View(categoryList);
+        }
+
+        // GET - CREATE
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
